3eef9793d08472310fb372956e5bef2282b2672e
Igor Gunia <vmta@yahoo.com>
2017-11-25 01:51:11 +0200

    genesis block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -124,9 +124,10 @@ public:
         //genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
 
-        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str()); // just to get a genesis block hash
+        printf("genesis.GetMerkleRoot = %s\n", genesis.GetMerkleRoot().ToString().c_str()); // just to get a genesis block hash
 
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
+        assert(consensus.hashGenesisBlock == uint256S("0x5aacffb4117b3ee50751738b384530d93ede3197f2c8b21ad115720d27f5c347"));
+        //assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
         assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
