4547e1ae1d81fbbf858dc884409cf764961da0af
Igor Gunia <vmta@yahoo.com>
2017-11-24 10:53:40 +0200

    Minor cleanup

--- src/net.cpp
+++ src/net.cpp
@@ -1647,17 +1647,6 @@ void CConnman::ThreadDNSAddressSeed()
     LogPrintf("%d addresses found from DNS seeds\n", found);
 }
 
-
-
-
-
-
-
-
-
-
-
-
 void CConnman::DumpAddresses()
 {
     int64_t nStart = GetTimeMillis();
