deb379258c64a894dfded64770b8f80d20f11d2a
Igor Gunia <vmta@yahoo.com>
2018-07-24 13:55:35 +0300

    Sync to mainstream commit 7ebd8c6385253bfa30175169a22f93474d8ddfaf (Mon, 23 Jul 2018 17:22:58 +0200)
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/umkoind.cpp
+++ src/umkoind.cpp
@@ -29,11 +29,13 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Umkoin (https://www.umkoin.org/),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Umkoin,
  * which enables instant payments to anyone, anywhere in the world. Umkoin uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
  * The software is a community-driven open source project, released under the MIT license.
+ *
+ * See https://github.com/umkoin/umkoin and http://umkoin.org/ for further information about the project.
  *
  * \section Navigation
  * Use the buttons <code>Namespaces</code>, <code>Classes</code> or <code>Files</code> at the top of the page to start navigating the code.
