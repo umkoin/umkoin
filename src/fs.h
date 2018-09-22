72328a4fb94c6f44fe2d03c9ae1dfa0663207f98
Igor Gunia <vmta@yahoo.com>
2018-09-23 02:27:34 +0300

    Sync to mainstream commit c64128df5882e8dc1f76ae7c1e998ed57b8645fe (Tue, 18 Sep 2018 01:20:49 +0200)
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/fs.h
+++ src/fs.h
@@ -18,28 +18,7 @@ namespace fs = boost::filesystem;
 /** Bridge operations to C stdio */
 namespace fsbridge {
     FILE *fopen(const fs::path& p, const char *mode);
-
-    class FileLock
-    {
-    public:
-        FileLock() = delete;
-        FileLock(const FileLock&) = delete;
-        FileLock(FileLock&&) = delete;
-        explicit FileLock(const fs::path& file);
-        ~FileLock();
-        bool TryLock();
-        std::string GetReason() { return reason; }
-
-    private:
-        std::string reason;
-#ifndef WIN32
-        int fd = -1;
-#else
-        void* hFile = (void*)-1; // INVALID_HANDLE_VALUE
-#endif
-    };
-
-    std::string get_filesystem_error_message(const fs::filesystem_error& e);
+    FILE *freopen(const fs::path& p, const char *mode, FILE *stream);
 };
 
 #endif // UMKOIN_FS_H
