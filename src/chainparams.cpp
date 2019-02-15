af368905ea44ca02d442d17e3dca52e66d3cbcd2
Igor Gunia <vmta@yahoo.com>
2019-02-15 08:49:39 +0200

    Sync to mainstream commit 38989ab03fb74ad56c1018c37cb24d17cbb3a16f (Thu, 14 Feb 2019 08:32:57 -0500)
    Merge #15183: [Qt]: fixes m_assumed_blockchain_size variable value
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -316,7 +316,7 @@ public:
 
         checkpointData = {
             {
-                {0, uint256S("0x60b48405c3efa2bd7aa6a8bc206e3369e58d15f60cba7a915769e9669758cb6d")}
+                {0, uint256S("0x60b48405c3efa2bd7aa6a8bc206e3369e58d15f60cba7a915769e9669758cb6d")},
             }
         };
 
