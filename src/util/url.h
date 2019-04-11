d42c0a4757f80921e9017cfa77db3e95f8cfe587
Igor Gunia <vmta@yahoo.com>
2019-04-11 14:47:11 +0300

    Sync to mainstream commit 6a135fbe5b30c3603616589b4779b1ce602c9392 (Wed, 10 Apr 2019 15:44:41 +0200)
    Merge #15638: Move-only: Pull wallet code out of libbitcoin_server
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/util/url.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2015-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_UTIL_URL_H
+#define UMKOIN_UTIL_URL_H
+
+#include <string>
+
+std::string urlDecode(const std::string &urlEncoded);
+
+#endif // UMKOIN_UTIL_URL_H
