0201e8f02916e4db9bb7a2914f87e4f14060e678
Igor Gunia <vmta@yahoo.com>
2019-04-11 14:49:56 +0300

    Sync to mainstream commit c536dfbcb00fb15963bf5d507b7017c241718bf6 (Thu, 11 Apr 2019 21:29:33 +1200)
    Merge #15639: bitcoin-wallet tool: Drop libbitcoin_server.a dependency
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/policy/settings.h
+++ src/policy/settings.h
@@ -6,12 +6,30 @@
 #ifndef UMKOIN_POLICY_SETTINGS_H
 #define UMKOIN_POLICY_SETTINGS_H
 
+#include <policy/policy.h>
+
 class CFeeRate;
+class CTransaction;
 
 // Policy settings which are configurable at runtime.
 extern CFeeRate incrementalRelayFee;
 extern CFeeRate dustRelayFee;
 extern unsigned int nBytesPerSigOp;
 extern bool fIsBareMultisigStd;
+
+static inline bool IsStandardTx(const CTransaction& tx, std::string& reason)
+{
+    return IsStandardTx(tx, ::fIsBareMultisigStd, ::dustRelayFee, reason);
+}
+
+static inline int64_t GetVirtualTransactionSize(int64_t weight, int64_t sigop_cost)
+{
+    return GetVirtualTransactionSize(weight, sigop_cost, ::nBytesPerSigOp);
+}
+
+static inline int64_t GetVirtualTransactionSize(const CTransaction& tx, int64_t sigop_cost)
+{
+    return GetVirtualTransactionSize(tx, sigop_cost, ::nBytesPerSigOp);
+}
 
 #endif // UMKOIN_POLICY_SETTINGS_H
