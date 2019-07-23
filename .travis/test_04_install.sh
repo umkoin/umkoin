e672fcf5fa9a2ce3efce92c948db3b5b26b52da6
Igor Gunia <vmta@yahoo.com>
2019-07-23 15:53:27 +0300

    Fix path to QA-assets.

--- .travis/test_04_install.sh
+++ .travis/test_04_install.sh
@@ -12,7 +12,7 @@ echo "Number of CPUs (nproc): $(nproc)"
 travis_retry docker pull "$DOCKER_NAME_TAG"
 
 export DIR_FUZZ_IN=${TRAVIS_BUILD_DIR}/qa-assets
-git clone https://github.com/umkoin/qa-assets ${DIR_FUZZ_IN}
+git clone https://github.com/bitcoin-core/qa-assets ${DIR_FUZZ_IN}
 export DIR_FUZZ_IN=${DIR_FUZZ_IN}/fuzz_seed_corpus/
 
 mkdir -p "${TRAVIS_BUILD_DIR}/sanitizer-output/"
