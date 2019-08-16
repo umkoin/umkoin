64e9c82c8a9c51b636d1e243e1d488287a7a26b2
Igor Gunia <vmta@yahoo.com>
2019-08-16 22:00:04 +0300

    Sync to mainstream commit 8fc7f0cba9b1b60ce25d6a8ba51052af1461f825 (Thu, 15 Aug 2019 15:03:41 +0800)
    Merge #16578: Do not pass in command line arguments to QApplication
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/test/test_main.cpp
+++ src/qt/test/test_main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
 
     // Don't remove this, it's needed to access
     // QApplication:: and QCoreApplication:: in the tests
-    UmkoinApplication app(*node, argc, argv);
+    UmkoinApplication app(*node);
     app.setApplicationName("Umkoin-Qt-test");
 
     AppTests app_tests(app);
--- src/qt/umkoin.h
+++ src/qt/umkoin.h
@@ -56,7 +56,7 @@ class UmkoinApplication: public QApplication
 {
     Q_OBJECT
 public:
-    explicit UmkoinApplication(interfaces::Node& node, int &argc, char **argv);
+    explicit UmkoinApplication(interfaces::Node& node);
     ~UmkoinApplication();
 
 #ifdef ENABLE_WALLET
