f780354e944adb0325ee3ff3e235f528aa6da2ef
Igor Gunia <vmta@yahoo.com>
2019-09-25 09:46:34 +0300

    Sync to mainstream commit 36604b4ef568aaf131f11c2b10e8c39e7a04b464 (Tue, 24 Sep 2019 11:32:34 -0400)
    Merge #16912: doc: Remove Doxygen intro from src/bitcoind.cpp
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/umkoind.cpp
+++ src/umkoind.cpp
@@ -25,24 +25,6 @@
 
 const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
 
-/* Introduction text for doxygen: */
-
-/*! \mainpage Developer documentation
- *
- * \section intro_sec Introduction
- *
- * This is the developer documentation of the reference client for an experimental new digital currency called Umkoin,
- * which enables instant payments to anyone, anywhere in the world. Umkoin uses peer-to-peer technology to operate
- * with no central authority: managing transactions and issuing money are carried out collectively by the network.
- *
- * The software is a community-driven open source project, released under the MIT license.
- *
- * See https://github.com/umkoin/umkoin and http://www.umkoin.org/ for further information about the project.
- *
- * \section Navigation
- * Use the buttons <code>Namespaces</code>, <code>Classes</code> or <code>Files</code> at the top of the page to start navigating the code.
- */
-
 static void WaitForShutdown()
 {
     while (!ShutdownRequested())
