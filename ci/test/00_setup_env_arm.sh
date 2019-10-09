ed1b693d0b5f015849daf86d4a7bc05c0e66ee95
Igor Gunia <vmta@yahoo.com>
2019-10-09 03:17:20 +0300

    Sync to mainstream commit 34919e00333f0e85c19819d18fd53ae5c7ff8855 (Tue, 8 Oct 2019 11:30:52 +0200)
    Merge #17011: ci: Use busybox utils for one build
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- ci/test/00_setup_env_arm.sh
+++ ci/test/00_setup_env_arm.sh
@@ -7,7 +7,11 @@
 export LC_ALL=C.UTF-8
 
 export HOST=arm-linux-gnueabihf
-export PACKAGES="python3 g++-arm-linux-gnueabihf"
+export PACKAGES="python3 g++-arm-linux-gnueabihf busybox"
+export USE_BUSY_BOX=true
+export RUN_UNIT_TESTS=false
+export RUN_FUNCTIONAL_TESTS=false
+export GOAL="install"
 # -Wno-psabi is to disable ABI warnings: "note: parameter passing for argument of type ... changed in GCC 7.1"
 # This could be removed once the ABI change warning does not show up by default
 export UMKOIN_CONFIG="--enable-glibc-back-compat --enable-reduce-exports CXXFLAGS=-Wno-psabi --disable-gui-tests --disable-tests --disable-bench"
