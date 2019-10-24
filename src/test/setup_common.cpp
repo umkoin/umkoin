afa6e07c3b7bf668db768d01a58db51a190772a7
Igor Gunia <vmta@yahoo.com>
2019-10-24 15:33:36 +0300

    Fix CSV and SegWit hardcoding issues

--- src/test/setup_common.cpp
+++ src/test/setup_common.cpp
@@ -125,7 +125,7 @@ TestChain100Setup::TestChain100Setup() : TestingSetup(CBaseChainParams::REGTEST)
 {
     // CreateAndProcessBlock() does not support building SegWit blocks, so don't activate in these tests.
     // TODO: fix the code to support SegWit blocks.
-    gArgs.ForceSetArg("-segwitheight", "0");
+    gArgs.ForceSetArg("-segwitheight", "432");
     SelectParams(CBaseChainParams::REGTEST);
 
     // Generate a 100-block chain:
