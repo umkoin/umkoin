4419cbcdb25a48d2f0310678f84841137f5eee84
Igor Gunia <vmta@yahoo.com>
2019-11-06 15:41:26 +0200

    Sync to mainstream commit 50591f6ec61b802cf4193cdbefcc85ad75716e8d (Tue, 5 Nov 2019 12:54:42 -0500)
    Merge #17357: tests: Add fuzzing harness for Bech32 encoding/decoding
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/test/util/str.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_TEST_UTIL_STR_H
+#define UMKOIN_TEST_UTIL_STR_H
+
+#include <string>
+
+bool CaseInsensitiveEqual(const std::string& s1, const std::string& s2);
+
+#endif // UMKOIN_TEST_UTIL_STR_H
