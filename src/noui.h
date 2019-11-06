7a9046a846a1fd02c7e0df106f09b890e98cb327
Igor Gunia <vmta@yahoo.com>
2019-11-06 15:56:30 +0200

    Sync to mainstream commit fea532a5f20c4d34f95855506d4fffed67ac4687 (Tue, 5 Nov 2019 14:31:41 -0500)
    Merge #16540: test: Add ASSERT_DEBUG_LOG to unit test framework
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/noui.h
+++ src/noui.h
@@ -17,10 +17,10 @@ void noui_InitMessage(const std::string& message);
 /** Connect all umkoind signal handlers */
 void noui_connect();
 
-/** Suppress all umkoind signal handlers. Used to suppress output during test runs that produce expected errors */
-void noui_suppress();
+/** Redirect all umkoind signal handlers to LogPrintf. Used to check or suppress output during test runs that produce expected errors */
+void noui_test_redirect();
 
-/** Reconnects the regular Non-GUI handlers after having used noui_suppress */
+/** Reconnects the regular Non-GUI handlers after having used noui_test_redirect */
 void noui_reconnect();
 
 #endif // UMKOIN_NOUI_H
