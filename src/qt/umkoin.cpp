a2866f9a69d54f939ea745136e457226c271901e
Igor Gunia <vmta@yahoo.com>
2019-12-18 16:31:54 +0200

    Sync to mainstream commit f9fd3a27fda3da3eb45de4e46e7145506e27d96b (Mon, 16 Dec 2019 16:07:12 -0500)
    Merge #17750: util: change GetWarnings parameter to bool
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/umkoin.cpp
+++ src/qt/umkoin.cpp
@@ -135,7 +135,7 @@ UmkoinCore::UmkoinCore(interfaces::Node& node) :
 void UmkoinCore::handleRunawayException(const std::exception *e)
 {
     PrintExceptionContinue(e, "Runaway exception");
-    Q_EMIT runawayException(QString::fromStdString(m_node.getWarnings("gui")));
+    Q_EMIT runawayException(QString::fromStdString(m_node.getWarnings()));
 }
 
 void UmkoinCore::initialize()
@@ -589,10 +589,10 @@ int GuiMain(int argc, char* argv[])
         }
     } catch (const std::exception& e) {
         PrintExceptionContinue(&e, "Runaway exception");
-        app.handleRunawayException(QString::fromStdString(node->getWarnings("gui")));
+        app.handleRunawayException(QString::fromStdString(node->getWarnings()));
     } catch (...) {
         PrintExceptionContinue(nullptr, "Runaway exception");
-        app.handleRunawayException(QString::fromStdString(node->getWarnings("gui")));
+        app.handleRunawayException(QString::fromStdString(node->getWarnings()));
     }
     return rv;
 }
