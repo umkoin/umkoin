62b424bc10c3318f1e1f041458f2dcf3d326fe1f
Igor Gunia <vmta@yahoo.com>
2020-01-30 09:28:21 +0200

    Sync to mainstream commit 01fc5891fb5702a744184af362da2850fd38038e (Wed, 29 Jan 2020 13:53:46 +0100)
    Merge #16702: p2p: supplying and using asmap to improve IP bucketing in addrman
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/util/asmap.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_UTIL_ASMAP_H
+#define UMKOIN_UTIL_ASMAP_H
+
+uint32_t Interpret(const std::vector<bool> &asmap, const std::vector<bool> &ip);
+
+#endif // UMKOIN_UTIL_ASMAP_H
