d353c33f764c1d24d6472aaf363333c0c748b46e
Igor Gunia <vmta@yahoo.com>
2020-07-19 16:24:37 +0300

    Sync to mainstream commit 6a53c3e390666e37322d6cddd8af2b09b750463d (Thu, 16 Jul 2020 09:55:12 +0200)
    Merge umkoin-core/gui#14: scripted-diff: rename movie folder to animation
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/umkoingui.cpp
+++ src/qt/umkoingui.cpp
@@ -1040,7 +1040,7 @@ void UmkoinGUI::setNumBlocks(int count, const QDateTime& blockDate, double nVeri
         if(count != prevBlocks)
         {
             labelBlocksIcon->setPixmap(platformStyle->SingleColorIcon(QString(
-                ":/movies/spinner-%1").arg(spinnerFrame, 3, 10, QChar('0')))
+                ":/animation/spinner-%1").arg(spinnerFrame, 3, 10, QChar('0')))
                 .pixmap(STATUSBAR_ICONSIZE, STATUSBAR_ICONSIZE));
             spinnerFrame = (spinnerFrame + 1) % SPINNER_FRAMES;
         }
