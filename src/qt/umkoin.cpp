99dc36833fdb8da2edd36cef778a68537a59587b
Igor Gunia <vmta@yahoo.com>
2020-08-30 21:08:11 +0300

    Sync to mainstream commit baf9cedee89f34a391b11240984c27b162bccdba (Sat, 29 Aug 2020 10:03:41 +0200)
    Merge #18817: doc: Document differences in umkoind and umkoin-qt locale handling
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/umkoin.cpp
+++ src/qt/umkoin.cpp
@@ -206,6 +206,7 @@ UmkoinApplication::UmkoinApplication():
     returnValue(0),
     platformStyle(nullptr)
 {
+    // Qt runs setlocale(LC_ALL, "") on initialization.
     RegisterMetaTypes();
     setQuitOnLastWindowClosed(false);
 }
