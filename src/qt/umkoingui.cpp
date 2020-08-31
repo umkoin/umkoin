45b34dd8c3331d5cb846a00bc20f805f0c6a266a
Igor Gunia <vmta@yahoo.com>
2020-08-31 16:48:10 +0300

    Sync to mainstream commit 269a7ccb2784aca12d911368e223c7691880cf22 (Mon, 31 Aug 2020 10:10:52 +0200)
    Merge #19099: refactor: Move wallet methods out of chain.h and node.h
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/umkoingui.cpp
+++ src/qt/umkoingui.cpp
@@ -1189,7 +1189,7 @@ void UmkoinGUI::incomingTransaction(const QString& date, int unit, const CAmount
     // On new transaction, make an info balloon
     QString msg = tr("Date: %1\n").arg(date) +
                   tr("Amount: %1\n").arg(UmkoinUnits::formatWithUnit(unit, amount, true));
-    if (m_node.getWallets().size() > 1 && !walletName.isEmpty()) {
+    if (m_node.walletClient().getWallets().size() > 1 && !walletName.isEmpty()) {
         msg += tr("Wallet: %1\n").arg(walletName);
     }
     msg += tr("Type: %1\n").arg(type);
