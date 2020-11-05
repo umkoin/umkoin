5fc5f34cf24966236dffca5c3dc990fbc2957c36
Igor Gunia <vmta@yahoo.com>
2020-11-05 16:23:51 +0200

    Sync to mainstream commit 95bde34a7186edb559efeeea7f2fbc0fbdd06273 (Tue, 3 Nov 2020 15:03:27 +0100)
    Merge #20237: net: Hardcoded seeds update for 0.21
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -15,7 +15,7 @@ NSEEDS=512
 
 MAX_SEEDS_PER_ASN=2
 
-MIN_BLOCKS = 74000
+MIN_BLOCKS = 100000
 
 # These are hosts that have been observed to be behaving strangely (e.g.
 # aggressively connecting to every node).
@@ -34,7 +34,8 @@ PATTERN_AGENT = re.compile(
     r"0.17.(0|0.1|1|2|99)|"
     r"0.18.(0|1|99)|"
     r"0.19.(0|1|99)|"
-    r"0.20.99"
+    r"0.20.(0|1|99)|"
+    r"0.21.99"
     r")")
 
 def parseline(line):
