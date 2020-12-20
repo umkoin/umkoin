9df9b1b4485889aec1302d55792679f2139f48fa
Igor Gunia <vmta@yahoo.com>
2020-12-20 18:59:59 +0200

    Sync to mainstream commit 5f2f304cbec9b8103c8a03fe6b6e55cb22865398 (Thu, 10 Dec 2020 20:22:54 +0100)
    build: Bump version to 0.19.2rc1
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- build_msvc/umkoin_config.h
+++ build_msvc/umkoin_config.h
@@ -17,7 +17,7 @@
 #define CLIENT_VERSION_MINOR 19
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 1
+#define CLIENT_VERSION_REVISION 2
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s developers"
@@ -346,7 +346,7 @@
 #define PACKAGE_NAME "Umkoin Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Umkoin Core 0.18.99"
+#define PACKAGE_STRING "Umkoin Core 0.19.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "umkoin"
@@ -355,7 +355,7 @@
 #define PACKAGE_URL "http://www.umkoin.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.18.99"
+#define PACKAGE_VERSION "0.19.2"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
