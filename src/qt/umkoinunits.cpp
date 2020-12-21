f850aefb162ca94c12e73f2727867332de650a0b
Igor Gunia <vmta@yahoo.com>
2020-12-21 09:13:52 +0200

    Sync to mainstream commit d0e76b5050a2f4af94022b7a1dc137a54ab801ce (Thu, 17 Dec 2020 10:53:31 +0100)
    Merge umkoin-core/gui#153: Define MAX_DIGITS_BTC for magic number in UmkoinUnits::format
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/umkoinunits.cpp
+++ src/qt/umkoinunits.cpp
@@ -8,6 +8,8 @@
 
 #include <cassert>
 
+static constexpr auto MAX_DIGITS_UMK = 16;
+
 UmkoinUnits::UmkoinUnits(QObject *parent):
         QAbstractListModel(parent),
         unitlist(availableUnits())
@@ -108,7 +110,9 @@ QString UmkoinUnits::format(int unit, const CAmount& nIn, bool fPlus, SeparatorS
     qint64 n_abs = (n > 0 ? n : -n);
     qint64 quotient = n_abs / coin;
     QString quotient_str = QString::number(quotient);
-    if (justify) quotient_str = quotient_str.rightJustified(16 - num_decimals, ' ');
+    if (justify) {
+        quotient_str = quotient_str.rightJustified(MAX_DIGITS_UMK - num_decimals, ' ');
+    }
 
     // Use SI-style thin space separators as these are locale independent and can't be
     // confused with the decimal marker.
