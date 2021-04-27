5e951712f5a9aa05b3860eb7402ed0c863211e2a
Igor Gunia <vmta@yahoo.com>
2021-04-27 14:39:05 +0300

    Sync to mainstream commit ac219dcbcc18a48210d58cdfaf7cf27ca5e7435b (Tue, 27 Apr 2021 10:17:19 +0200)
    Merge umkoin/umkoin#19160: multiprocess: Add basic spawn and IPC support
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/ipc/capnp/init-types.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_IPC_CAPNP_INIT_TYPES_H
+#define UMKOIN_IPC_CAPNP_INIT_TYPES_H
+
+#include <ipc/capnp/echo.capnp.proxy-types.h>
+
+#endif // UMKOIN_IPC_CAPNP_INIT_TYPES_H
