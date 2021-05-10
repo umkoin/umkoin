2fb9f0dffca866fcbb120b032618e12a028f2067
Igor Gunia <vmta@yahoo.com>
2021-05-10 18:56:52 +0300

    Sync to mainstream commit 2a22d903f3da54b31d21e74c3e084f5113f13789 (Mon, 10 May 2021 08:43:59 +0200)
    Merge umkoin/umkoin#21897: rpc: adjust incorrect RPCHelpMan types
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -540,7 +540,7 @@ static RPCHelpMan listaddressgroupings()
                     {
                         {RPCResult::Type::ARR, "", "",
                         {
-                            {RPCResult::Type::ARR, "", "",
+                            {RPCResult::Type::ARR_FIXED, "", "",
                             {
                                 {RPCResult::Type::STR, "address", "The umkoin address"},
                                 {RPCResult::Type::STR_AMOUNT, "amount", "The amount in " + CURRENCY_UNIT},
