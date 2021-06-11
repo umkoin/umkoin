3190f8765ee1e30c271207cb5bd11a581a29b691
Igor Gunia <vmta@yahoo.com>
2021-06-11 13:15:03 +0300

    Sync to mainstream commit f66eceaecf464bfab5e19f3ca8fe680d8a6aa2e1 (Fri, 11 Jun 2021 09:18:28 +0200)
    Merge umkoin/umkoin#22216: refactor: Make SetupServerArgs callable without NodeContext
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/umkoind.cpp
+++ src/umkoind.cpp
@@ -112,8 +112,8 @@ static bool AppInit(NodeContext& node, int argc, char* argv[])
     util::ThreadSetInternalName("init");
 
     // If Qt is used, parameters/umkoin.conf are parsed in qt/umkoin.cpp's main()
-    SetupServerArgs(node);
     ArgsManager& args = *Assert(node.args);
+    SetupServerArgs(args);
     std::string error;
     if (!args.ParseParameters(argc, argv, error)) {
         return InitError(Untranslated(strprintf("Error parsing command line arguments: %s\n", error)));
