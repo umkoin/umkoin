c0c654eca1cb26f2a1e3450628eed4d717b4ce23
Igor Gunia <vmta@yahoo.com>
2021-07-16 09:15:56 +0300

    Sync to mainstream commit 21998bc028d6d72229ae826d2dd9d78981367952 (Thu, 15 Jul 2021 14:30:10 +0200)
    Merge umkoin/umkoin#22284: p2p, refactor: performance improvements to ProtectEvictionCandidatesByRatio()
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/test/util/net.h
+++ src/test/util/net.h
@@ -141,4 +141,6 @@ private:
     mutable size_t m_consumed;
 };
 
+std::vector<NodeEvictionCandidate> GetRandomNodeEvictionCandidates(int n_candidates, FastRandomContext& random_context);
+
 #endif // UMKOIN_TEST_UTIL_NET_H
