a4513ce26dc7343d32c5c117f56cc91ff931a092
Igor Gunia <vmta@yahoo.com>
2021-12-15 18:43:39 +0000

    Sync to mainstream commit fa3fb46b8172597cb64ff401b27f6c799fb832d7 (Wed, 8 Dec 2021 11:08:57 +0800)
    Merge umkoin/umkoin#23667: Split up rpcwallet
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/wallet/rpc/wallet.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2016-2020 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_WALLET_RPC_WALLET_H
+#define UMKOIN_WALLET_RPC_WALLET_H
+
+#include <span.h>
+
+class CRPCCommand;
+
+Span<const CRPCCommand> GetWalletRPCCommands();
+
+#endif // UMKOIN_WALLET_RPC_WALLET_H
