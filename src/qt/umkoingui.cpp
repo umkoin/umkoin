047b8ba3b7f49b44abc7f1e247870a635a13851e
Igor Gunia <vmta@yahoo.com>
2022-04-13 14:59:26 +0300

    Sync to mainstream commit f60a63cc5f16b738d9d2ada3f10b27cf999df323 (Wed, 13 Apr 2022 01:22:33 +0200)
    Merge umkoin-core/gui#577: Getting ready to Qt 6 (1/n)
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/umkoingui.cpp
+++ src/qt/umkoingui.cpp
@@ -41,6 +41,7 @@
 #include <validation.h>
 
 #include <QAction>
+#include <QActionGroup>
 #include <QApplication>
 #include <QComboBox>
 #include <QCursor>
@@ -355,7 +356,7 @@ void UmkoinGUI::createActions()
     showHelpMessageAction->setStatusTip(tr("Show the %1 help message to get a list with possible Umkoin command-line options").arg(PACKAGE_NAME));
 
     m_mask_values_action = new QAction(tr("&Mask values"), this);
-    m_mask_values_action->setShortcut(QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_M));
+    m_mask_values_action->setShortcut(QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_M));
     m_mask_values_action->setStatusTip(tr("Mask the values in the Overview tab"));
     m_mask_values_action->setCheckable(true);
 
@@ -426,8 +427,8 @@ void UmkoinGUI::createActions()
     }
 #endif // ENABLE_WALLET
 
-    connect(new QShortcut(QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_C), this), &QShortcut::activated, this, &UmkoinGUI::showDebugWindowActivateConsole);
-    connect(new QShortcut(QKeySequence(Qt::CTRL + Qt::SHIFT + Qt::Key_D), this), &QShortcut::activated, this, &UmkoinGUI::showDebugWindow);
+    connect(new QShortcut(QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_C), this), &QShortcut::activated, this, &UmkoinGUI::showDebugWindowActivateConsole);
+    connect(new QShortcut(QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_D), this), &QShortcut::activated, this, &UmkoinGUI::showDebugWindow);
 }
 
 void UmkoinGUI::createMenuBar()
