d75101c29c543650d07a09784660a372812a32b5
Igor Gunia <vmta@yahoo.com>
2022-05-09 15:35:36 +0300

    Sync to mainstream commit 12455acca2c3adf5c88ae9c1a02a7c192fe0f53b (Tue, 3 May 2022 10:31:21 +0200)
    Merge umkoin/umkoin#24470: Disallow more unsafe string->path conversions allowed by path append operators
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -508,7 +508,7 @@ fs::path static StartupShortcutPath()
         return GetSpecialFolderPath(CSIDL_STARTUP) / "Umkoin.lnk";
     if (chain == CBaseChainParams::TESTNET) // Remove this special case when CBaseChainParams::TESTNET = "testnet4"
         return GetSpecialFolderPath(CSIDL_STARTUP) / "Umkoin (testnet).lnk";
-    return GetSpecialFolderPath(CSIDL_STARTUP) / strprintf("Umkoin (%s).lnk", chain);
+    return GetSpecialFolderPath(CSIDL_STARTUP) / fs::u8path(strprintf("Umkoin (%s).lnk", chain));
 }
 
 bool GetStartOnSystemStartup()
@@ -589,7 +589,7 @@ fs::path static GetAutostartFilePath()
     std::string chain = gArgs.GetChainName();
     if (chain == CBaseChainParams::MAIN)
         return GetAutostartDir() / "umkoin.desktop";
-    return GetAutostartDir() / strprintf("umkoin-%s.desktop", chain);
+    return GetAutostartDir() / fs::u8path(strprintf("umkoin-%s.desktop", chain));
 }
 
 bool GetStartOnSystemStartup()
