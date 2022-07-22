af8fba6849dbaa5f1abacd64c06441efb72ff94a
Igor Gunia <vmta@yahoo.com>
2022-07-22 15:18:12 +0300

    Sync to mainstream commit 86133df7a2be80203fa439bf344e1f45dc2be41d (Fri, 22 Jul 2022 08:07:37 +0200)
    Merge umkoin/umkoin#25662: contrib: prune valgrind suppressions
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- ci/test/00_setup_env_native_valgrind.sh
+++ ci/test/00_setup_env_native_valgrind.sh
@@ -14,4 +14,4 @@ export NO_DEPENDS=1
 export TEST_RUNNER_EXTRA="--nosandbox --exclude feature_init,rpc_bind,feature_bind_extra"  # Excluded for now
 export GOAL="install"
 # Temporarily pin dwarf 4, until valgrind can understand clang's dwarf 5
-export UMKOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=no CC=clang CXX=clang++ CXXFLAGS='-fdebug-default-version=4'"  # TODO enable GUI
+export UMKOIN_CONFIG="--enable-zmq --with-incompatible-bdb --with-gui=no CC=clang CXX=clang++ CFLAGS='-gdwarf-4' CXXFLAGS='-gdwarf-4'"  # TODO enable GUI
