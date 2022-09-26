7ba77e57bf28ed9556e24c88a1d7bce76b5da4e1
Igor Gunia <vmta@yahoo.com>
2022-09-26 11:19:09 +0300

    Sync to mainstream commit ec13810a007669518a98a464cd540bc37ec7e306 (Sun, 25 Sep 2022 11:45:17 +0000)
    Merge umkoin/umkoin#26159: build: Remove `stdlib.h` from header checks
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -6,7 +6,7 @@
 #ifndef UMKOIN_CONSENSUS_CONSENSUS_H
 #define UMKOIN_CONSENSUS_CONSENSUS_H
 
-#include <stdlib.h>
+#include <cstdlib>
 #include <stdint.h>
 
 /** The maximum allowed size for a serialized block, in bytes (only for buffer size limits) */
--- src/crypto/sha256.h
+++ src/crypto/sha256.h
@@ -5,8 +5,8 @@
 #ifndef UMKOIN_CRYPTO_SHA256_H
 #define UMKOIN_CRYPTO_SHA256_H
 
+#include <cstdlib>
 #include <stdint.h>
-#include <stdlib.h>
 #include <string>
 
 /** A hasher class for SHA-256. */
