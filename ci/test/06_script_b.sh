3c5f7724d565591884dc8ee4ac7a0f310872f793
Igor Gunia <vmta@yahoo.com>
2022-10-13 08:40:01 +0300

    Sync to mainstream commit 9ca39d69dfe03c19662867c3d53a5e6e41277d4a (Mon, 10 Oct 2022 18:08:35 +0200)
    Merge umkoin/umkoin#26254: iwyu: Add zmq source files
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- ci/test/06_script_b.sh
+++ ci/test/06_script_b.sh
@@ -68,6 +68,7 @@ if [ "${RUN_TIDY}" = "true" ]; then
           " src/util/string.cpp"\
           " src/util/syserror.cpp"\
           " src/util/url.cpp"\
+          " src/zmq"\
           " -p . ${MAKEJOBS} -- -Xiwyu --cxx17ns -Xiwyu --mapping_file=${BASE_BUILD_DIR}/umkoin-$HOST/contrib/devtools/iwyu/umkoin.core.imp"
 fi
 
