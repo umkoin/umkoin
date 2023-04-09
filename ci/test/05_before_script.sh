4e379b347dc0b7a1bb4a39385e2fe545b332a2da
Igor Gunia <vmta@yahoo.com>
2023-04-09 11:36:35 +0300

    Sync to mainstream commit 5c2bb2b54cc7fceef4b9db631536ee6ed8129864 (Fri, 31 Mar 2023 13:14:28 +0100)
    Merge umkoin/umkoin#27376: ci: Remove second user account
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- ci/test/05_before_script.sh
+++ ci/test/05_before_script.sh
@@ -11,7 +11,6 @@ if [ "$CI_OS_NAME" == "macos" ]; then
   echo > "${HOME}/Library/Application Support/Umkoin"
 else
   CI_EXEC echo \> \$HOME/.umkoin
-  CI_EXEC_ROOT echo \> \$HOME/.umkoin
 fi
 
 if [ -z "$NO_DEPENDS" ]; then
