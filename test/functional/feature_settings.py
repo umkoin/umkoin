1e2419e2a4453b8d2d9154aad380b8aa609165b8
Igor Gunia <vmta@yahoo.com>
2023-07-01 12:01:20 +0300

    Sync to mainstream commit e8543629ae4355fdba28eea974394d61a44d1071 (Thu, 29 Jun 2023 09:36:15 +0100)
    Merge umkoin/umkoin#27884: test: Use TestNode datadir_path or chain_path where possible
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- test/functional/feature_settings.py
+++ test/functional/feature_settings.py
@@ -21,7 +21,7 @@ class SettingsTest(UmkoinTestFramework):
 
     def run_test(self):
         node, = self.nodes
-        settings = Path(node.datadir, self.chain, "settings.json")
+        settings = Path(node.chain_path, "settings.json")
         conf = Path(node.datadir, "umkoin.conf")
 
         # Assert empty settings file was created
