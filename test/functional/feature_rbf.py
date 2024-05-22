c4826ac9bcd4a039503b2c6afd16c8505dcacae2
Igor Gunia <vmta@yahoo.com>
2024-05-22 14:10:57 +0300

    Sync to mainstream commit ecd23656db174adef61d3bd753d02698c3528192 (Mon, 20 May 2024 09:46:49 +0100)
    Merge umkoin/umkoin#30133: test: remove unneeded `-maxorphantx=1000` settings
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- test/functional/feature_rbf.py
+++ test/functional/feature_rbf.py
@@ -28,7 +28,6 @@ class ReplaceByFeeTest(UmkoinTestFramework):
         self.num_nodes = 2
         self.extra_args = [
             [
-                "-maxorphantx=1000",
                 "-limitancestorcount=50",
                 "-limitancestorsize=101",
                 "-limitdescendantcount=200",
