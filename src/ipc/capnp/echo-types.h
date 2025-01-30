8d142665e40603e9f6d85facf23ab4cd33a4dbaf
Igor Gunia <vmta@yahoo.com>
2025-01-30 14:04:12 +0200

    Sync to mainstream commit ad2f9324c619ae608c434ee85ad9ae1b2812877d (Wed, 29 Jan 2025 12:10:23 +0000)
    Merge umkoin/umkoin#31740: depends: Update libmultiprocess library before converting to subtree
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/ipc/capnp/echo-types.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_IPC_CAPNP_ECHO_TYPES_H
+#define UMKOIN_IPC_CAPNP_ECHO_TYPES_H
+
+#include <mp/type-context.h>
+#include <mp/type-decay.h>
+#include <mp/type-string.h>
+
+#endif // UMKOIN_IPC_CAPNP_ECHO_TYPES_H
