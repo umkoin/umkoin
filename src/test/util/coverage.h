3272d1bd54729a48d84a544eeb68108183dbaade
Igor Gunia <vmta@yahoo.com>
2025-03-14 11:32:12 +0200

    Sync to mainstream commit c20a5ce106be716a503bcf615a1900ba8c635430 (Thu, 13 Mar 2025 12:30:32 +0800)
    Merge umkoin/umkoin#31901: contrib: Add deterministic-unittest-coverage
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ src/test/util/coverage.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2025-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UMKOIN_TEST_UTIL_COVERAGE_H
+#define UMKOIN_TEST_UTIL_COVERAGE_H
+
+void ResetCoverageCounters();
+
+#endif // UMKOIN_TEST_UTIL_COVERAGE_H
