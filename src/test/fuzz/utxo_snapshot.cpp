c4d30fa2fc0a43180f9f5e3539caa0378a0f1ace
Igor Gunia <vmta@yahoo.com>
2025-09-10 00:20:57 +0300

    Sync to mainstream commit 12fb00fd4228ad5158fd4893e7804d5667ea700b (Fri, 11 Jul 2025 11:18:03 +0100)
    Merge umkoin/umkoin#32927: fuzz: Add missing calls to `SetMockTime` for determinism
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/test/fuzz/utxo_snapshot.cpp
+++ src/test/fuzz/utxo_snapshot.cpp
@@ -72,6 +72,7 @@ void initialize_chain()
     const auto params{CreateChainParams(ArgsManager{}, ChainType::REGTEST)};
     static const auto chain{CreateBlockChain(2 * COINBASE_MATURITY, *params)};
     g_chain = &chain;
+    SetMockTime(chain.back()->Time());
 
     // Make sure we can generate a valid snapshot.
     sanity_check_snapshot();
@@ -102,7 +103,7 @@ void utxo_snapshot_fuzz(FuzzBufferType buffer)
 {
     SeedRandomStateForTest(SeedRand::ZEROS);
     FuzzedDataProvider fuzzed_data_provider(buffer.data(), buffer.size());
-    SetMockTime(ConsumeTime(fuzzed_data_provider, /*min=*/1511512200)); // regtest genesis block timestamp
+    SetMockTime(ConsumeTime(fuzzed_data_provider, /*min=*/1296688602)); // regtest genesis block timestamp
     auto& setup{*g_setup};
     bool dirty_chainman{false}; // Reuse the global chainman, but reset it when it is dirty
     auto& chainman{*setup.m_node.chainman};
