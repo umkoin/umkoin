041f9529f847d6dd7397fd967c1d7842e106d488
Igor Gunia <vmta@yahoo.com>
2025-09-10 00:42:42 +0300

    Sync to mainstream commit afd3b34dc5e2fcd31004b6e3caca7df1e6ed66ba (Wed, 23 Jul 2025 09:57:05 +0100)
    Merge umkoin/umkoin#33004: Enable `-natpmp` by default
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/mapport.h
+++ src/mapport.h
@@ -5,7 +5,7 @@
 #ifndef UMKOIN_MAPPORT_H
 #define UMKOIN_MAPPORT_H
 
-static constexpr bool DEFAULT_NATPMP = false;
+static constexpr bool DEFAULT_NATPMP = true;
 
 void StartMapPort(bool enable);
 void InterruptMapPort();
