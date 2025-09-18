612e2a3efe4d195be06868c35a596de20d133040
Igor Gunia <vmta@yahoo.com>
2025-09-18 08:24:47 +0300

    Sync to mainstream commit e04cb9c1bdf2199127cc8cf9c87f25e46b8cac7b (Fri, 5 Sep 2025 13:00:46 -0400)
    Merge umkoin/umkoin#33201: Add functional test for IPC interface
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- test/functional/test_framework/test_framework.py
+++ test/functional/test_framework/test_framework.py
@@ -76,9 +76,9 @@ class Binaries:
         self.paths = paths
         self.bin_dir = bin_dir
 
-    def node_argv(self):
+    def node_argv(self, **kwargs):
         "Return argv array that should be used to invoke umkoind"
-        return self._argv("node", self.paths.umkoind)
+        return self._argv("node", self.paths.umkoind, **kwargs)
 
     def rpc_argv(self):
         "Return argv array that should be used to invoke umkoin-cli"
@@ -101,16 +101,19 @@ class Binaries:
         "Return argv array that should be used to invoke umkoin-chainstate"
         return self._argv("chainstate", self.paths.umkoinchainstate)
 
-    def _argv(self, command, bin_path):
+    def _argv(self, command, bin_path, need_ipc=False):
         """Return argv array that should be used to invoke the command. It
-        either uses the umkoin wrapper executable (if UMKOIN_CMD is set), or
-        the direct binary path (umkoind, etc). When bin_dir is set (by tests
-        calling binaries from previous releases) it always uses the direct
-        path."""
+        either uses the umkoin wrapper executable (if UMKOIN_CMD is set or
+        need_ipc is True), or the direct binary path (umkoind, etc). When
+        bin_dir is set (by tests calling binaries from previous releases) it
+        always uses the direct path."""
         if self.bin_dir is not None:
             return [os.path.join(self.bin_dir, os.path.basename(bin_path))]
-        elif self.paths.umkoin_cmd is not None:
-            return self.paths.umkoin_cmd + [command]
+        elif self.paths.umkoin_cmd is not None or need_ipc:
+            # If the current test needs IPC functionality, use the umkoin
+            # wrapper binary and append -m so it calls multiprocess binaries.
+            umkoin_cmd = self.paths.umkoin_cmd or [self.paths.umkoin_bin]
+            return umkoin_cmd + (["-m"] if need_ipc else []) + [command]
         else:
             return [bin_path]
 
@@ -158,6 +161,7 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
         self.noban_tx_relay: bool = False
         self.nodes: list[TestNode] = []
         self.extra_args = None
+        self.extra_init = None
         self.network_thread = None
         self.rpc_timeout = 60  # Wait for up to 60 seconds for the RPC server to respond
         self.supports_cli = True
@@ -278,6 +282,7 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
 
         paths = types.SimpleNamespace()
         binaries = {
+            "umkoin": "UMKOIN_BIN",
             "umkoind": "UMKOIND",
             "umkoin-cli": "UMKOINCLI",
             "umkoin-util": "UMKOINUTIL",
@@ -285,6 +290,8 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
             "umkoin-chainstate": "UMKOINCHAINSTATE",
             "umkoin-wallet": "UMKOINWALLET",
         }
+        # Set paths to umkoin core binaries allowing overrides with environment
+        # variables.
         for binary, env_variable_name in binaries.items():
             default_filename = os.path.join(
                 self.config["environment"]["BUILDDIR"],
@@ -550,15 +557,15 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
 
             bin_dirs.append(bin_dir)
 
+        extra_init = [{}] * num_nodes if self.extra_init is None else self.extra_init # type: ignore[var-annotated]
+        assert_equal(len(extra_init), num_nodes)
         assert_equal(len(extra_confs), num_nodes)
         assert_equal(len(extra_args), num_nodes)
         assert_equal(len(versions), num_nodes)
         assert_equal(len(bin_dirs), num_nodes)
         for i in range(num_nodes):
             args = list(extra_args[i])
-            test_node_i = TestNode(
-                i,
-                get_datadir_path(self.options.tmpdir, i),
+            init = dict(
                 chain=self.chain,
                 rpchost=rpchost,
                 timewait=self.rpc_timeout,
@@ -575,6 +582,11 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
                 v2transport=self.options.v2transport,
                 uses_wallet=self.uses_wallet,
             )
+            init.update(extra_init[i])
+            test_node_i = TestNode(
+                i,
+                get_datadir_path(self.options.tmpdir, i),
+                **init)
             self.nodes.append(test_node_i)
             if not test_node_i.version_is_at_least(170000):
                 # adjust conf for pre 17
@@ -952,6 +964,13 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
         except ImportError:
             raise SkipTest("sqlite3 module not available.")
 
+    def skip_if_no_py_capnp(self):
+        """Attempt to import the capnp package and skip the test if the import fails."""
+        try:
+            import capnp  # type: ignore[import] # noqa: F401
+        except ImportError:
+            raise SkipTest("capnp module not available.")
+
     def skip_if_no_python_bcc(self):
         """Attempt to import the bcc package and skip the tests if the import fails."""
         try:
@@ -1016,6 +1035,11 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
         if not self.is_cli_compiled():
             raise SkipTest("umkoin-cli has not been compiled.")
 
+    def skip_if_no_ipc(self):
+        """Skip the running test if ipc is not compiled."""
+        if not self.is_ipc_compiled():
+            raise SkipTest("ipc has not been compiled.")
+
     def skip_if_no_previous_releases(self):
         """Skip the running test if previous releases are not available."""
         if not self.has_previous_releases():
@@ -1074,6 +1098,10 @@ class UmkoinTestFramework(metaclass=UmkoinTestMetaClass):
     def is_usdt_compiled(self):
         """Checks whether the USDT tracepoints were compiled."""
         return self.config["components"].getboolean("ENABLE_USDT_TRACEPOINTS")
+
+    def is_ipc_compiled(self):
+        """Checks whether ipc was compiled."""
+        return self.config["components"].getboolean("ENABLE_IPC")
 
     def has_blockfile(self, node, filenum: str):
         return (node.blocks_path/ f"blk{filenum}.dat").is_file()
