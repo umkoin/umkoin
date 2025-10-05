8f9f24dad1efd8e9d9193faf1335a4868984b3f7
Igor Gunia <vmta@yahoo.com>
2025-10-05 19:13:30 +0300

    Sync to mainstream commit acc7f2a433b131597124ba0fbbe9952c4d36a872 (Wed, 1 Oct 2025 11:35:56 +0100)
    Merge umkoin/umkoin#33401: ci: Remove bash -c from cmake invocation using eval
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- ci/test/03_test_script.sh
+++ ci/test/03_test_script.sh
@@ -130,7 +130,8 @@ if [[ "${RUN_TIDY}" == "true" ]]; then
   UMKOIN_CONFIG_ALL="$UMKOIN_CONFIG_ALL -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
 fi
 
-bash -c "cmake -S $BASE_ROOT_DIR -B ${BASE_BUILD_DIR} $UMKOIN_CONFIG_ALL $UMKOIN_CONFIG" || (
+eval "CMAKE_ARGS=($UMKOIN_CONFIG_ALL $UMKOIN_CONFIG)"
+cmake -S "$BASE_ROOT_DIR" -B "$BASE_BUILD_DIR" "${CMAKE_ARGS[@]}" || (
   cd "${BASE_BUILD_DIR}"
   # shellcheck disable=SC2046
   cat $(cmake -P "${BASE_ROOT_DIR}/ci/test/GetCMakeLogFiles.cmake")
