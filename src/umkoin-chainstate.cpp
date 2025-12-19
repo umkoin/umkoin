a4f354f643245041e40ca618c5955aabe56bbc6d
Igor Gunia <vmta@yahoo.com>
2025-12-19 16:01:29 +0200

    Sync to mainstream commit 9272fd517fd3fac98063809a4cc576edea82ca82 (Thu, 18 Dec 2025 15:16:00 +0000)
    Merge umkoin/umkoin#34105: kernel: revert accidentally removed copyright header
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/umkoin-chainstate.cpp
+++ src/umkoin-chainstate.cpp
@@ -1,3 +1,16 @@
+// Copyright (c) 2022-present The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+//
+// The umkoin-chainstate executable serves to surface the dependencies required
+// by a program wishing to use Umkoin Core's consensus engine as it is right
+// now.
+//
+// DEVELOPER NOTE: Since this is a "demo-only", experimental, etc. executable,
+//                 it may diverge from Umkoin Core's coding style.
+//
+// It is part of the libumkoinkernel project.
+
 #include <kernel/umkoinkernel_wrapper.h>
 
 #include <cassert>
