3635d1323c4d1c6897a7fe3241460df276a51a1d
Igor Gunia <vmta@yahoo.com>
2026-02-06 19:24:33 +0200

    Sync to mainstream commit 8bb77f348ef390b7f7c7fb6b57fdc7e86ddb4ce7 (Sat, 31 Jan 2026 15:05:44 +0100)
    Merge umkoin/umkoin#34455: ci, iwyu: Fix warnings in `src/univalue` and treat them as errors
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/univalue/lib/univalue.cpp
+++ src/univalue/lib/univalue.cpp
@@ -1,5 +1,5 @@
 // Copyright 2014 BitPay Inc.
-// Copyright 2015 Bitcoin Core Developers
+// Copyright 2015 Umkoin Core Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://opensource.org/licenses/mit-license.php.
 
@@ -7,7 +7,6 @@
 
 #include <iomanip>
 #include <map>
-#include <memory>
 #include <sstream>
 #include <string>
 #include <utility>
--- src/univalue/lib/univalue_get.cpp
+++ src/univalue/lib/univalue_get.cpp
@@ -1,15 +1,11 @@
 // Copyright 2014 BitPay Inc.
-// Copyright 2015 Bitcoin Core Developers
+// Copyright 2015 Umkoin Core Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or https://opensource.org/licenses/mit-license.php.
 
 #include <univalue.h>
 
-#include <cerrno>
-#include <cstdint>
-#include <cstdlib>
 #include <cstring>
-#include <limits>
 #include <locale>
 #include <sstream>
 #include <stdexcept>
