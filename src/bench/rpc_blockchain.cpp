2877a1b02a03513a05ff65da7fe8fa6968fddd14
Igor Gunia <vmta@yahoo.com>
2026-04-22 14:56:54 +0300

    Sync to mainstream commit e32bc7f817e7adeffef22a985bf8dbd4aa3e08d8 (Tue, 21 Apr 2026 13:51:39 +0200)
    Merge umkoin/umkoin#35025: refactor: use `SpanReader` in deserialization benchmarks
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/bench/rpc_blockchain.cpp
+++ src/bench/rpc_blockchain.cpp
@@ -31,10 +31,7 @@ struct TestBlockAndIndex {
 
     TestBlockAndIndex()
     {
-        DataStream stream{benchmark::data::block47789};
-        std::byte a{0};
-        stream.write({&a, 1}); // Prevent compaction
-
+        SpanReader stream{benchmark::data::block47789};
         stream >> TX_WITH_WITNESS(block);
 
         blockHash = block.GetHash();
