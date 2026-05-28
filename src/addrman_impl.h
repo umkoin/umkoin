e6351c7a7119be9c61fe8237397eef0799f8209e
Igor Gunia <vmta@yahoo.com>
2026-05-28 22:35:33 +0300

    Sync to mainstream commit 735b1cf4311de81cc6877a9c2efc082e861ef8d5 (Thu, 21 May 2026 22:05:18 -0400)
    Merge umkoin/umkoin#34806: refactor: logging: Various API improvements
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- src/addrman_impl.h
+++ src/addrman_impl.h
@@ -5,13 +5,13 @@
 #ifndef UMKOIN_ADDRMAN_IMPL_H
 #define UMKOIN_ADDRMAN_IMPL_H
 
-#include <logging.h>
 #include <logging/timer.h>
 #include <netaddress.h>
 #include <protocol.h>
 #include <serialize.h>
 #include <sync.h>
 #include <uint256.h>
+#include <util/log.h>
 #include <util/time.h>
 
 #include <cstdint>
