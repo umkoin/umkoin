a302d6153e10340209d7a53ce33c09207c63e49a
Igor Gunia <vmta@yahoo.com>
2026-06-10 17:17:31 +0300

    Sync to mainstream commit 10dfdd4b9fab578e43ba211325967cbc28cb6652 (Fri, 29 May 2026 15:16:32 +0100)
    Merge umkoin/umkoin#35379: test: Fix feature_dbcrash.py --usecli error
    
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- ci/test/00_setup_env_native_alpine_musl.sh
+++ ci/test/00_setup_env_native_alpine_musl.sh
@@ -17,5 +17,5 @@ export UMKOIN_CONFIG="\
  -DREDUCE_EXPORTS=ON \
  -DCMAKE_BUILD_TYPE=Debug \
 "
-export TEST_RUNNER_EXTRA="--v2transport --usecli --extended"
+export TEST_RUNNER_EXTRA="--v2transport --usecli --extended --exclude feature_dbcrash"  # Run extended tests under --usecli and --v2transport, but exclude the very slow dbcrash
 export UMKOIN_CMD="umkoin -m" # Used in functional tests
